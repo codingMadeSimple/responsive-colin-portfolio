cfa8db4d6a159cbf515eb37637b2d14089d007e1
Colin Spangelo <colin.spangelo@gmail.com>
2024-04-26 11:18:41 -0600

    Streak.

--- portfolio/src/components/Statistics.jsx
+++ portfolio/src/components/Statistics.jsx
@@ -34,7 +34,7 @@ const Statistics = () => {
           <div className="stats-card">
             <FontAwesomeIcon icon={faArrowTrendUp} />
             <div>
-              <p >Current Commit Streak: 8 Days</p>
+              <p >Current Commit Streak: 9 Days</p>
             </div>
           </div>
           <div className="stats-card-resume">
